8987658310fe2ed242a6a25af07ba68a96ccd228
amanda <amanda.v.barros6@aluno.senai.br>
2025-04-15 11:08:05 -0500

    alterações

---
+++ ferragensjoao/script.js
@@ -0,0 +1,13 @@
+let martelo = 19.99;
+
+//pegar o valor do campo produto
+
+//pegar o valor do campo qtd
+
+
+function p(){
+    let escolhaproduto = document.getElementById("produto").Value;
+    console.log(escolhaproduto);
+    let Quantidadedeproduto = document.getElementById("Quantidade").Value;
+    console.log(Quantidadedeproduto)
+}  
