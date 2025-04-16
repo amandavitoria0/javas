2f2e9e709cb5feb6a419a130b207e2ffbcfa33f2
amanda <amanda.v.barros6@aluno.senai.br>
2025-04-16 09:52:06 -0500

    continuação

--- ferragensjoao/script.js
+++ ferragensjoao/script.js
@@ -3,11 +3,16 @@ let martelo = 19.99;
 //pegar o valor do campo produto
 
 //pegar o valor do campo qtd
+const precos={//dicionario
+    "parafuso": 5,
+    "martelo": 30,
+    "chave de fenda":38,
+}
 
 
 function p(){
     let escolhaproduto = document.getElementById("produto").Value;
-    console.log(escolhaproduto);
     let Quantidadedeproduto = document.getElementById("Quantidade").Value;
-    console.log(Quantidadedeproduto)
+    console.log(preços[escolhaproduto]*Quantidadedeproduto)
+    
 }  
