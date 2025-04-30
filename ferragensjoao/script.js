543a42e6628aafd4c81b4db61c588c5f23cf6753
amanda <amanda.v.barros6@aluno.senai.br>
2025-04-30 10:36:15 -0500

    alterações

--- ferragensjoao/script.js
+++ ferragensjoao/script.js
@@ -7,14 +7,6 @@ const precos={//dicionario
     "chave de fenda":38,
 }
 
-{mostrarResultado()
-    //valores
-    let valor1 = 30;
-    let valor2 = 150;
-    let valor3 = 152;
-    let resultado = valor1 + valor2 + valor3;
-}
-
 
 function calcularpreco(){
     let escolhaproduto = document.getElementById("produto").value; //
